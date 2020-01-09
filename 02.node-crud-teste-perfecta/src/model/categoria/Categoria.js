2fdfded44733545c5b29c2aff5a076245ddf346c
albertrein <guilhermealberto8@gmail.com>
2020-01-08 23:32:18 -0300

    Creating event value to safe changes on database.

--- 02.node-crud-teste-perfecta/src/model/categoria/Categoria.js
+++ 02.node-crud-teste-perfecta/src/model/categoria/Categoria.js
@@ -2,7 +2,10 @@ const firebase = require('../Firebase');
 
 module.exports = class Categoria{
 	constructor(){
-		
+		const databaseReference = firebase.ref('perfecta');
+		databaseReference.on('value', snapshot => {
+			console.log('>>',snapshot);
+		});
 	}
 
 	createCategory(categoryName){
