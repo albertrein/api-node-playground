cb960dcd24b1f0575ad8fabc73f09ed619dec2c0
albertrein <guilhermealberto8@gmail.com>
2020-01-14 00:00:57 -0300

    Finishing job crud basic

--- 02.node-crud-teste-perfecta/src/model/job/Job.js
+++ 02.node-crud-teste-perfecta/src/model/job/Job.js
@@ -18,11 +18,17 @@ module.exports = class Jobs{
 
 	createJob(categoryName, jobTitle, jobDescription){
 		let sended = false;
+		
 		this.databaseReference.on('child_added', snap => {
 			sended = true;
 		});
 
-		this.databaseReference.child(jobTitle).set({"description":jobDescription});
+		firebase.ref('perfecta/perfecta-category').orderByChild('category').equalTo(categoryName).once('value', obj => {
+			if(!obj){
+				return false;
+			}
+			this.databaseReference.child(jobTitle).set({"jobCategory":categoryName, "description":jobDescription});
+		});
 		return sended;
 	}
 
@@ -35,35 +41,4 @@ module.exports = class Jobs{
 		return removed;
 	}
 
-	makeTestFilter(categoryName){
-		firebase.ref('perfecta/perfecta-category').orderByChild('category').equalTo(categoryName).once('value', obj => {
-			console.log(obj.val());
-		});
-		console.log('Dont');
-	}
-
-	readTest(){
-		firebase.ref('teste').once('value', snap => {
-			let objeto = snap.val();
-
-			for(let i in objeto){
-				console.log('>>',objeto[i].job);
-			}
-
-		})
-	}
-	deleteTest(){
-
-
-		firebase.ref('teste').once('value', snap => {
-			let objeto = snap.val();
-
-			for(let i in objeto){
-				if(objeto[i].job == "guilherme"){
-					firebase.ref('teste/'+i).remove();
-				}
-			}
-
-		})
-	}
 }
